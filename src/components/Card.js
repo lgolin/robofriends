b61ebf967d40bbec4d42073e11d0a99aa078aaad
Lais <laisgolin@gmail.com>
2021-08-02 12:18:14 +0100

    update cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,8 @@ import './card.css';
 const Card = ({ name, email, id }) => {
   return (
     // <div className="tc grow br3 pa3 ma2 dib bw2 shadow-5 card-bg">
-    <div className="dib br3 pa1 ma2 grow bw2 shadow-5 bg-light-green">
+    <div className="dib ma2 w-20 dib br3 grow shadow-5 bg-washed-green ">
+      {/* <div className="dib mw9 center ph3-ns cf ph2-ns fl w-100 w-25-ns pa2 tc pv4"> */}
       <img src={`https://robohash.org/${id}?size=200x200`} alt="robot-icon" />
       <div>
         <h2>{name}</h2>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div>
       <input
-        className="pa3 ba mb4 b--gray bg-washed-yellow"
+        className="pa3 bn br3 ba mb4 b--gray bg-washed-yellow"
         type="search"
         placeholder="search your robot"
         onChange={searchChange}
